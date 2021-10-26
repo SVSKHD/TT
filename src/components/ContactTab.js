3f977d7a98d7d85f528f486c1c7aebf002571c04
hiteshSVSK <hiteshsunkara@eworldes.com>
2021-10-26 19:52:42 +0530

    contact changes

--- src/components/ContactTab.js
+++ src/components/ContactTab.js
@@ -6,7 +6,7 @@ const ContactTab = () => {
   return (
     <ButtonGroup>
       <Button  color="warning" target="_blank" href="tel:9100400245" ><FaPhone size={30}/></Button>
-      <Button color="success" target="_blank" href="https://wa.me/9100400245?text=We Need your Taxi services"><FaWhatsapp size={30}/></Button>
+      <Button color="success" target="_blank" href="https://wa.me/+919100400245?text=We Need your Taxi services"><FaWhatsapp size={30}/></Button>
     </ButtonGroup>
   );
 }
